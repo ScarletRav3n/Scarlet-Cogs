d5ba9c0c1832ee095983eb65ffa65ad337bc705f
ScarletRav3n <scarletrav3n@gmail.com>
2016-10-09 02:46:26 -0500

    Created CatFacts.py & json

---
+++ catfacts/catfacts.py
@@ -0,0 +1,45 @@
+# noinspection PyUnresolvedReferences
+import discord
+from discord.ext import commands
+from random import choice as rnd
+from .utils.dataIO import dataIO
+import os
+
+__author__ = "ScarletRav3n"
+
+
+class CatFacts:
+    """Cat Facts"""
+
+    def __init__(self, bot):
+        self.bot = bot
+        self.cat = "data/catfacts/catfacts.json"
+        self.system = dataIO.load_json(self.cat)
+        # self.kaomoji = `data/kaomoji`
+
+    def save_emotes(self):
+        dataIO.save_json(self.cat, self.service)
+        dataIO.is_valid_json("data/catfacts/catfacts.json")
+
+    @commands.command(aliases=["cat"])
+    async def catfacts(self):
+        await self.bot.say(rnd(self.system))
+
+
+def check_folders():
+    if not os.path.exists("data/kaomoji"):
+        print("Creating data/catfacts folder...")
+        os.makedirs("data/catfacts")
+
+
+def check_files():
+    f = "data/catfacts/catfacts.json"
+    if not dataIO.is_valid_json(f):
+        print("No such thing as catfacts.json...")
+
+
+def setup(bot):
+    check_folders()
+    check_files()
+    n = CatFacts(bot)
+    bot.add_cog(n)
