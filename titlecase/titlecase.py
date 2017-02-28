654995ea6b4002e2689b7abb57619452edcac099
ScarletRaven <scarletrav3n@gmail.com>
2017-02-28 12:42:05 -0600

    Tweaked titlecase

--- titlecase/titlecase.py
+++ titlecase/titlecase.py
@@ -23,9 +23,9 @@ class TitleCase:
 
         def c(s): return [u[0] for u in re.findall(pattern, s)]
         trigger = str(c(m.content))
-        if m.author.bot is not True:
+        if m.author.bot is False:
             if trigger != "[]":
-                trigger = str(m.author.name) + ' wrote *"' + trigger[2:-2] + '"*'
+                trigger = str(m.author.name) + ' wrote *"' + trigger[2:-2] + '--"*'
                 try:
                     await self.bot.delete_message(m)
                 except discord.errors.Forbidden:
