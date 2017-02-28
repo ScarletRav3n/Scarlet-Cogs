26fc58f96e5af305a35ee54fd4688f1522feadb7
Leo Garcia <leodasher1@hotmail.com>
2017-02-28 16:46:39 -0600

    Major update to grammar.py
    Added regex to only trigger whole words. Blood no longer trigger 'lewd*' (from lood) is a good example. Created json to increase trigger count without worrying about the script itself. *farewell comments*

--- grammar/grammar.py
+++ grammar/grammar.py
@@ -2,10 +2,15 @@
 import discord
 from discord.ext import commands
 from cogs.utils import checks
+from .utils.dataIO import dataIO
+import re
+import os
 
 __author__ = "ScarletRav3n"
 
+
 # TODO: Find a better way to trigger a/an's
+# TODO: Allow self added fixes?
 
 
 class Grammar:
@@ -14,11 +19,17 @@ class Grammar:
     def __init__(self, bot):
         self.bot = bot
         self.toggle = False
+        self.grammar = "data/grammar/grammar.json"
+        self.system = dataIO.load_json(self.grammar)
+
+    def save_grammar(self):
+        dataIO.save_json(self.grammar, self.system)
+        dataIO.is_valid_json("data/grammar/grammar.json")
 
     @commands.command()
     @checks.admin_or_permissions(administrator=True)
     async def grammar(self, on_off: str):
-        """Toggle ^ caret"""
+        """Toggle ^ caret deletion"""
         if on_off.lower() == "on":
             await self.bot.say("Deleting carets is now ON." +
                                "\n`Make sure I have the 'manage_messages' " +
@@ -31,115 +42,31 @@ class Grammar:
             await self.bot.say("I need an ON or OFF state.")
 
     async def on_message(self, m):
-        str_m = m.content.lower()
         for x in self.bot.settings.get_prefixes(m.server):
-            if m.author.bot is not True:
+            if m.author.bot is False:
                 if m.content.startswith(x):
                     return
-                # elif " a a" in str_m: # a/an
-                #    fix = "an*"
-                # elif " a e" in str_m:
-                #    fix = "an*"
-                # elif " a o" in str_m:
-                #    fix = "an*"
-                # elif " a u" in str_m:
-                #    fix = "an*"
-                # elif " a i" in str_m:
-                #    fix = "an*"
-                elif "your a " in str_m:  # your/there
-                    fix = "you're*"
-                elif "your an " in str_m:
-                    fix = "you're*"
-                elif "your on " in str_m:
-                    fix = "you're*"
-                elif "their not" in str_m:
-                    fix = "they're*"
-                elif "their a " in str_m:
-                    fix = "they're*"
-                elif "their an " in str_m:
-                    fix = "they're*"
-                elif "theres a " in str_m:
-                    fix = "there's*"
-                elif "theres an " in str_m:
-                    fix = "there's*"
-                elif "dont " in str_m:  # apostrophes
-                    fix = "don't*"
-                elif "didnt " in str_m:
-                    fix = "didn't*"
-                elif "cant " in str_m:
-                    fix = "can't*"
-                elif "wont " in str_m:
-                    fix = "won't*"
-                elif "isnt " in str_m:
-                    fix = "isn't*"
-                elif "its not" in str_m:
-                    fix = "it's*"
-                elif "laif" in str_m:  # stupid broken english patch start here
-                    fix = "life*"
-                elif "stronk" in str_m:
-                    fix = "strong*"
-                elif "noice" in str_m:
-                    fix = "nice*"
-                elif "lood" in str_m:
-                    fix = "lewd*"
-                elif "taht" in str_m:
-                    fix = "that*"
-                elif "nawt" in str_m:
-                    fix = "not*"
-                elif "lern" in str_m:
-                    fix = "learn*"
-                elif "spel" in str_m:
-                    fix = "spell*"
-                elif "rood" in str_m:
-                    fix = "rude*"
-                # elif "gud" in str_m: # I CAN'T BELIEVE THIS IS REJECTED
-                    # fix = "good*" # IN FAVOR OF A MEME
-                elif "stahp" in str_m:
-                    fix = "stop*"
-                elif "tommorrow" in str_m:  # other english words
-                    fix = "tomorrow*"
-                elif "tomorow" in str_m:
-                    fix = "tomorrow*"
-                elif "begining" in str_m:
-                    fix = "beginning*"
-                elif "litteral" in str_m:
-                    fix = "literal*"
-                elif "rediculous" in str_m:
-                    fix = "ridiculous*"
-                elif "defiantly" in str_m:
-                    fix = "definitely*"
-                elif "definately" in str_m:
-                    fix = "definitely*"
-                elif "definitly" in str_m:
-                    fix = "definitely*"
-                elif "multible" in str_m:
-                    fix = "multiple*"
-                elif "becuase" in str_m:
-                    fix = "because*"
-                elif "mispell" in str_m:
-                    fix = "misspell*"
-                elif "indispensible" in str_m:
-                    fix = "indispensable*"
-                elif "personel" in str_m:
-                    fix = "personnel*"
-                elif "recieve" in str_m:
-                    fix = "receive*"
-                elif "treshold" in str_m:
-                    fix = "threshold*"
-                elif "untill" in str_m:
-                    fix = "until*"
-                elif "wierd" in str_m:
-                    fix = "weird*"
-                elif "^" in m.content:  # caret
-                    if self.toggle is True:
-                        try:
-                            await self.bot.delete_message(m)
-                        except discord.errors.Forbidden:
-                            await self.bot.say("Wanted to delete mid {} but no permissions".format(m.id))
-                    return
-                else:
-                    return
-                await self.bot.send_message(m.channel, fix)
+                for k, v in self.system.items():
+                    if re.findall(r'\b' + k + r'\b', m.content.lower()):
+                        await self.bot.send_message(m.channel, v)
+                    elif "^" in m.content:  # caret
+                        if self.toggle is True:
+                            try:
+                                await self.bot.delete_message(m)
+                            except discord.errors.Forbidden:
+                                await self.bot.say("Wanted to delete mid {} but no permissions".format(m.id))
+
+
+def check_folders():
+    if not os.path.exists("data/grammar"):
+        print("Creating data/grammar folder...")
+        os.makedirs("data/grammar")
+
+
+def check_files():
+    f = "data/grammar/grammar.json"
+    if not dataIO.is_valid_json(f):
+        print("No such thing as grammar.json...")
 
 
 def setup(bot):
